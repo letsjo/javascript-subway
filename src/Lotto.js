e76d525862a8b9d1e10dc36370a142e32898a4a4
letsjo <hyunoh.jo@gmail.com>
2022-11-13 21:52:06 +0900

    test(LottoTest): 당첨번호 유효성 테스트

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,15 +1,21 @@
+// 상수
+const MESSAGE = require('./constants/lottoMessage');
+// 오프젝트
+const Validator = require('./Validator');
+
 class Lotto {
   #numbers;
 
   constructor (numbers) {
-    this.validate(numbers);
     this.#numbers = numbers;
+    this.validateNumbers();
   }
 
-  validate (numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-    }
+  validateNumbers () {
+    const validateForArr = [...this.#numbers];
+    if (!Validator.isNumbersLength(validateForArr)) throw new Error(MESSAGE.ERROR.INVALID_LENGTH);
+    if (!Validator.isNumbersRange(validateForArr)) throw new Error(MESSAGE.ERROR.INVALID_DIGIT);
+    if (Validator.isDuplicates(validateForArr)) throw new Error(MESSAGE.ERROR.DUPLICATE_DIGIT);
   }
 
   // TODO: 추가 기능 구현
