7e5b67a57b42eaa81ebcab471e774e7db3f9c806
letsjo <hyunoh.jo@gmail.com>
2022-11-13 17:23:19 +0900

    style(*): 제공 파일 airbnb 가이드에 따라 스타일 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,14 +1,14 @@
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
+  constructor (numbers) {
     this.validate(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
+  validate (numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
