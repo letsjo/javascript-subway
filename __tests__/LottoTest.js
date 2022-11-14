5e9b8074cdd361c509c308bbdf6ecc1640758d13
letsjo <hyunoh.jo@gmail.com>
2022-11-14 13:09:05 +0900

    test(*): 기능2 구입금액만큼 로또 발급 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,28 @@
 const Lotto = require('../src/Lotto');
 const Accounting = require('../src/Accounting');
+const LotteryApp = require('../src/LotteryApp');
 const MESSAGE = require('../src/constants/lottoMessage');
+
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => acc.mockImplementationOnce((question, callback) => {
+    callback(input);
+  }), MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => acc
+    .mockReturnValueOnce(number), MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -40,5 +62,56 @@ describe('구입 금액 입력 테스트', () => {
     }).toThrow(MESSAGE.ERROR.REMAIN_CHANGE);
   });
 
+  // 아래에 추가 테스트 작성 가능
+});
+
+describe('로또 구매 테스트', () => {
+  test('구입 금액 1000원일 때 1장 발급', () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+    ]);
+
+    const logs = [
+      '1개를 구매했습니다.',
+      '[8, 21, 23, 41, 42, 43]',
+    ];
+    const logSpy = getLogSpy();
+    const lotteryApp = new LotteryApp();
+    lotteryApp.purchaseLottos(1000);
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test('구입 금액이 8000원일 때 8장 발급', () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+
+    const logs = [
+      '8개를 구매했습니다.',
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+      '[7, 11, 16, 35, 36, 44]',
+      '[1, 8, 11, 31, 41, 42]',
+      '[13, 14, 16, 38, 42, 45]',
+      '[7, 11, 30, 40, 42, 43]',
+      '[2, 13, 22, 32, 38, 45]',
+      '[1, 3, 5, 14, 22, 45]',
+    ];
+    const logSpy = getLogSpy();
+    const lotteryApp = new LotteryApp();
+    lotteryApp.purchaseLottos(8000);
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
   // 아래에 추가 테스트 작성 가능
 });
