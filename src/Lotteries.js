14c222791efc8055d038255876a310b233122858
letsjo <hyunoh.jo@gmail.com>
2022-11-14 12:40:09 +0900

    feat(Lotteries): Lotteries 로또 발행 메소드 생성

--- src/Lotteries.js
+++ src/Lotteries.js
@@ -1,6 +1,25 @@
+// 모듈
+const { Random } = require('@woowacourse/mission-utils');
+// 오브젝트
+const Lotto = require('./Lotto');
+
 class Lotteries {
   #storage;
+
   constructor () {
     this.#storage = [];
   }
+
+  purchaseAuto () {
+    this.#storage.push(new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6)));
+  }
+
+  getStorage () {
+    return this.#storage;
+  }
+  getSaleQty () {
+    return this.#storage.length;
+  }
 }
+
+module.exports = Lotteries;
