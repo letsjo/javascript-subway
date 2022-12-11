21e161ddc5a257dee5b5b581d55017a7cc6a9ae9
letsjo <hyunoh.jo@gmail.com>
2022-12-11 20:38:58 +0900

    feat(*): MVC 패턴으로 리펙토링을 위한 Views 생성

---
+++ src/views/InputView.js
@@ -0,0 +1,7 @@
+const { Console } = require('@woowacourse/mission-utils');
+const MESSAGE = require('../constants/lottoMessage');
+
+const InputView = {
+};
+
+module.exports = InputView;
---
+++ src/views/OutputView.js
@@ -0,0 +1,7 @@
+const { Console } = require('@woowacourse/mission-utils');
+const MESSAGE = require('../constants/lottoMessage');
+
+const OutputView = {
+};
+
+module.exports = OutputView;
