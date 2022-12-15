46cdf048bc9a3dead045c99632d5b5726d4c151a
letsjo <hyunoh.jo@gmail.com>
2022-12-16 05:36:04 +0900

    feat(Validator): 역이름 입력 유효성 검사 추가

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,5 +1,5 @@
-const MESSAGE = require('../constants/lottoMessage');
-const { LOTTO_INFO } = require('../constants/lottoSetting');
+const MESSAGE = require('../constants/subwayMessage');
+const { STATIONS } = require('../constants/subwaySetting');
 const handleError = require('./utils/handleError');
 
 class Validator {
@@ -8,6 +8,29 @@ class Validator {
       return handleError(errorMsg);
     }
     return true;
+  }
+
+  static validatorDepartureStation(station) {
+    return Validator.departureForm(station) && Validator.checkStation(station);
+  }
+
+  static validatorArrivalStation(station) {
+    return Validator.arrivalForm(station) && Validator.checkStation(station);
+  }
+
+  static departureForm(value) {
+    return this.#validate(/^[가-힣0-9]{2,}/.test(value), MESSAGE.ERROR.noDeparture);
+  }
+
+  static arrivalForm(value) {
+    return this.#validate(/^[가-힣0-9]{2,}/.test(value), MESSAGE.ERROR.noArrival);
+  }
+
+  static checkStation(inputStation) {
+    return this.#validate(
+      STATIONS.find(station => station === inputStation),
+      MESSAGE.ERROR.noStation,
+    );
   }
 }
 
