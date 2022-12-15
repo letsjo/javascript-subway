8578746ae2d356997b6499eef663684ca576117a
letsjo <hyunoh.jo@gmail.com>
2022-12-16 04:35:41 +0900

    feat(subwaySetting): 상수 파일 생성

---
+++ src/constants/subwaySetting.js
@@ -0,0 +1,92 @@
+const PROCESS_CONSTANTS = {
+  shortestDistance: 'D',
+  minimumTime: 'T',
+  retrySearch: 'R',
+  quitSearch: 'Q',
+};
+
+const STATIONS = [
+  {
+    name: '교대',
+  },
+  {
+    name: '강남',
+  },
+  {
+    name: '역삼',
+  },
+  {
+    name: '남부터미널',
+  },
+  {
+    name: '양재',
+  },
+  {
+    name: '양재시민의숲',
+  },
+  {
+    name: '매봉',
+  },
+];
+
+const LINES = [
+  {
+    name: '2호선',
+    sections: [
+      {
+        start: '교대',
+        end: '강남',
+        distance: 2,
+        time: 3,
+      },
+      {
+        start: '강남',
+        end: '역삼',
+        distance: 2,
+        time: 3,
+      },
+    ],
+  },
+  {
+    name: '3호선',
+    sections: [
+      {
+        start: '교대',
+        end: '남부터미널',
+        distance: 3,
+        time: 2,
+      },
+      {
+        start: '남부터미널',
+        end: '양재',
+        distance: 6,
+        time: 5,
+      },
+      {
+        start: '양재',
+        end: '매봉',
+        distance: 1,
+        time: 1,
+      },
+    ],
+  },
+  {
+    name: '신분당선',
+    sections: [
+      {
+        start: '강남',
+        end: '양재',
+        distance: 2,
+        time: 8,
+      },
+      {
+        start: '양재',
+        end: '양재시민의숲',
+        distance: 10,
+        time: 3,
+      },
+    ],
+  },
+];
+
+module.exports = { PROCESS_CONSTANTS, STATIONS, LINES };
